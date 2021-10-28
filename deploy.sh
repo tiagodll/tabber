956ad545eaba8041d900836c641ff7e70fe57059
Tiago Almeida Dall'Igna <tiago@dalligna.com>
2021-10-28 14:10:03 +0200

    favicon

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 cd src
 sudo docker stop tabber
-sudo docker build -t tabberimg . --rm
-sudo docker run -p 7000:7000 -t --rm \
+sudo docker build -t tabber . --rm
+sudo docker run -p 7000:7000 -t \
   --env ASPNETCORE_ENVIRONMENT=Production \
   --env ASPNETCORE_URLS=http://+:7000 \
-  --name tabber -d -it tabberimg
+  --name tabber -d --restart unless-stopped -it tabber
 cd ..
